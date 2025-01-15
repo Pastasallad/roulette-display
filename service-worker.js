4181eb6d72b67f30d9f98509854b52444f4e7cd3
Pastasallad <pastasallad@gmail.com>
2025-01-15 03:23:49 +0100

    Initial

---
+++ service-worker.js
@@ -0,0 +1,56 @@
+const cacheName = 'v2';
+const precachedResources = [
+  '/index.html',
+  '/css/style.css',
+  '/js/main.js',
+  '/image/logo.svg'
+];
+
+async function precache() {
+  const cache = await caches.open(cacheName);
+  return cache.addAll(precachedResources);
+}
+
+// Cache first with refresh
+function isCacheable(request) {
+  const url = new URL(request.url);
+  return !url.pathname.endsWith(".json");
+}
+
+async function cacheFirstWithRefresh(request) {
+  const fetchResponsePromise = fetch(request).then(async (networkResponse) => {
+    if (networkResponse.ok) {
+      const cache = await caches.open(cacheName);
+      cache.put(request, networkResponse.clone());
+    }
+    return networkResponse;
+  });
+
+  return (await caches.match(request)) || (await fetchResponsePromise);
+}
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(precache());
+});
+
+self.addEventListener('activate', e => {
+  console.log('Service Worker: Activated');
+  e.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== cacheName) {
+            console.log('Service Worker: Clearing old cache');
+            return caches.delete(cache);
+          }
+        })
+      )
+    })
+  )
+});
+
+self.addEventListener("fetch", (event) => {
+  if (isCacheable(event.request)) {
+    event.respondWith(cacheFirstWithRefresh(event.request));
+  }
+});
